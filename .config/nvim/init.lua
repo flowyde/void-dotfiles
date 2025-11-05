c1d821690835d4b9adce03915c418f49bb17b983
flowyde <69410739+flowyde@users.noreply.github.com>
2025-11-05 03:49:15 -0300

    added waybar configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.tabstop = 4      -- Define o número de espaços que um tab ocupa
+vim.opt.shiftwidth = 4   -- Define o número de espaços utilizados para identação
+vim.opt.expandtab = true -- Converte tabs em espaços
